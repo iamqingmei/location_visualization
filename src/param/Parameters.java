f53770af44aca444205781f2b44b6fca06e1ecc7
Administrator <Administrator@155.69.129.53>
2018-03-30 14:46:36 +0800

    Map height coor

--- src/param/Parameters.java
+++ src/param/Parameters.java
@@ -1,8 +1,8 @@
 package param;
 
 public class Parameters {
-	public static float MAP_MAXHEIGHT_COOR = 10;
-	public static float MAP_MAXWIDTH_COOR = 10;
+	public static float MAP_MAXHEIGHT_COOR = 5;
+	public static float MAP_MAXWIDTH_COOR = 5;
 	public static float MAP_PIXEL_MULTIPLIER = 35;
 	public static int MAP_MARGIN = 25;
 }
